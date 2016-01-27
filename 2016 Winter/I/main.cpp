82de063ba0e6a84adecd669a9714ce0303d784fb
Henry Tseng <projectstowork1@gmail.com>
2016-01-27 10:17:33 +0000

    Start I

---
+++ 2016 Winter/I/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+vector<int> g[2]
+
+int main()
+{
+    int ncase;
+    scanf("%d", &ncase);
+
+    while(ncase--) {
+        int n;
+        scanf("%d", &n);
+
+        for(int i = 0; i < n - 1; i++) {
+            
+        }
+    }
+    return 0;
+}
